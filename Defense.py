ec89af90993f483fb22a6f1d561a6e84c6a0cc4d
RootLess <eygwork@gmail.com>
2019-02-13 17:18:31 +0200

    defense added, some minor changes, updates from webkit

---
+++ Defense.py
@@ -0,0 +1,12 @@
+from elf_kingdom import *
+
+class Defence:
+    def __init__(self, game, elfDict):
+        self.game = game
+        self.elfDict = elfDict
+
+    def do_defence(self, game, elfDict):
+        self.elfDict = elfDict
+        self.game = game
+        danger_close = 2000  # distance from enemy portals that's considered to close
+        elf_portal_pairs = [(elf, portal) for elf, portal in elfDict.values(), game.get_enemy_portals()]
