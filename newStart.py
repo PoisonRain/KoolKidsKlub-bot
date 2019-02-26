0ce56894afd17a1a5a72af8747e9a3671c024260
PoisonRain <32774691+PoisonRain@users.noreply.github.com>
2019-02-26 17:37:28 +0200

    Update newStart.py

--- newStart.py
+++ newStart.py
@@ -1,5 +1,6 @@
 from elf_kingdom import *
 from newMath import *
+import Elf
 
 locs = []
 
@@ -23,13 +24,17 @@ class Start:
                 pos_point = get_point_by_alpha(pos_alpha % 360, start_location, end_location)
                 if self.can_build_portals_by_list(game, pos_point, target_points):
                     target_points.append(pos_point)
-                    print pos_point
+                    print 'pos_point: '+str(pos_point)
+                    if len(target_points) >= amount:
+                        break
                     
                 pos_alpha += 5
                 neg_point = get_point_by_alpha(neg_alpha % 360, start_location, end_location)
                 if self.can_build_portals_by_list(game, neg_point, target_points):
                     target_points.append(neg_point)
-                    print neg_point
+                    print 'neg_point: '+str(neg_point)
+                    if len(target_points) >= amount:
+                        break
                     
                 neg_alpha -= 5
                 
@@ -50,17 +55,22 @@ class Start:
         return True
         
     
-    def build_structure_ring(self, locs, my_elves, elfDict):
-        for i in xrange(len(my_elves)):
-            if i < len(locs):
-                if elfDict[my_elves[i].unique_id].build_portal(locs[i]):
-                    locs.remove(locs[i])
+    def build_structure_ring(self, locs, elfDict):
+        elfDict = dict(elfDict)
+        for loc in locs:
+            worker_elf = Elf.Elf.get_closest_elf(loc, elfDict)
+            did_build = worker_elf.build_portal(loc)
+            elfDict.pop(worker_elf, None)
+            if did_build:
+                locs.remove(loc)
+        return locs
+                
 
 """ add the following code to do_turn for testing """
 #  strtI = newStart.Start()
 #     global defense_portal_locs
 #     if game.turn == 1:
-#         defense_portal_locs = strtI.get_structure_ring_locations(game, elfDict[my_elves[0].unique_id], 1000, 10)
+#         defense_portal_locs = strtI.get_structure_ring_locations(game, elfDict[my_elves[0].unique_id], 1000, 4)
     
 #     if defense_portal_locs != []:
-#         strtI.build_structure_ring(defense_portal_locs, my_elves, elfDict)
+#         strtI.build_structure_ring(defense_portal_locs, elfDict)
