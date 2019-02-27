89a2d3c127f54f43105a2e5cccead1f1b9259725
RootLess <eygwork@gmail.com>
2019-02-27 16:18:50 +0200

    reverting math

--- newMath.py
+++ newMath.py
@@ -1,4 +1,5 @@
 from flanking import tuple_to_location, location_to_tuple
+from elf_kingdom import Location
 import math
 
 
@@ -14,16 +15,16 @@ def get_alpha_from_points(center_point, trgt_point):
     center_point = location_to_tuple(center_point)
     trgt_point = location_to_tuple(trgt_point)
 
-    a = math.acos(trgt_point[0] / (center_point[0] + radius))
+    a = math.asin((trgt_point[1] - center_point[1]) / radius)
+    if math.cos(a) < 0:
+        return 180-math.degrees(a)
+    return math.degrees(a)
 
-    if trgt_point[1] > center_point[1]:
-        return math.degrees(a)
-    return 360 - math.degrees(a)
 
 
 def get_point_by_alpha(alpha, center_point, trgt_point):
     """
-    returns a point on the circle at a certain angle
+    returns a point on the cir_cle at a certain angle
     :param alpha: the angle
     :param center_point: the center of the circle
     :param trgt_point: a point on the circle
@@ -31,11 +32,11 @@ def get_point_by_alpha(alpha, center_point, trgt_point):
     """
     radius = center_point.distance(trgt_point)
     center_point = location_to_tuple(center_point)
-    alpha = math.radians(alpha)
+    #alpha = math.radians(alpha)
     x = center_point[0] + radius * math.cos(alpha)
     y = center_point[1] + radius * math.sin(alpha)
 
-    tup = (int(x), int(y))
+    tup = (-int(x), int(y))
 
     return tuple_to_location(tup)
 
@@ -49,10 +50,11 @@ def move_point_by_angle(axis, point, angle_delta):
     :return: the new point
     """
     angle = get_alpha_from_points(axis, point)
+    print angle
     angle += angle_delta
     return get_point_by_alpha(math.radians(angle), axis, point)
 
-#print move_point_by_angle(Location(0,0), Location(0,1), 90), (1, 0)
-#print move_point_by_angle(Location(0,0), Location(1,0), 90), (0, -1)
-#print move_point_by_angle(Location(0,0), Location(0,-1), 90), (-1, 0)
-#print move_point_by_angle(Location(0,0), Location(-1,0), 90), (0, 1)
+print move_point_by_angle(Location(0,0), Location(0,1), 90), (1, 0)
+print move_point_by_angle(Location(0,0), Location(1,0), 90), (0, -1)
+print move_point_by_angle(Location(0,0), Location(0,-1), 90), (-1, 0)
+print move_point_by_angle(Location(0,0), Location(-1,0), 90), (0, 1)
