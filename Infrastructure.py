28ea8352b4535d8737eb484906071ef3e1d09ad2
RootLess <eygwork@gmail.com>
2019-03-01 00:24:46 +0200

    REEEEEEEEEEEEEEEEEEE

--- Infrastructure.py
+++ Infrastructure.py
@@ -7,7 +7,7 @@ __metaclass__ = type
 
 
 class Infrastructure(Start):
-    def __init__(self, game, elfDict, start_distance=800, radius=1000, ziging_angle=2):
+    def __init__(self, game, elfDict, start_distance=800, radius=1000, ziging_angle=30):
         """
         the class builds portals systematicly along the game board
         :param game: game instence
@@ -36,28 +36,29 @@ class Infrastructure(Start):
         self.game = game
         self.elfDict = elfDict
         self.update_portals(self.not_built)
-        self.portal_locations = list( dict.fromkeys(self.portal_locations))
+
+    def remove_duplicates(self):
+        for location in self.portal_locations:
+            for location_other in self.portal_locations:
+                if location is not location and location.equals(location_other):
+                    self.portal_locations.remove(location_other)
+        print self.portal_locations
 
     def update_portals(self, not_built):
         """
         updates self.built_portals
         :param not_built: a list of bortals that are yet builded
         """
-        #try:
+        # try:
         built_loccations = list(self.portal_locations)
-        print "before", built_loccations
         for location in not_built:
             built_loccations.remove(location)
-        print "after", built_loccations
         built_portals = []
         for location in built_loccations:
             portal = Portals.portal_on_location(self.game, location)
             if portal:
                 built_portals.append(portal)
-        print "final", built_portals
-        self.built_portals = built_portals
-        #except Exception as msg:
-        #    print msg, "update_portals"
+        self.built_portals = built_portals  # except Exception as msg:  #    print msg, "update_portals"
 
     def get_next_location(self):
         """
@@ -69,14 +70,21 @@ class Infrastructure(Start):
         game = self.game
         my_castle = game.get_my_castle()
         enemy_castle = game.get_enemy_castle()
-        target_location = get_point_by_alpha(self.ziging_angle, my_castle.location, enemy_castle.location)
+        target_location = enemy_castle.location
         start_location = my_castle.location.towards(target_location, self.radius)
-        location = self.get_object_ring_locations(game, my_castle.location, start_location, 1, 0)
+
+        # start_location = get_point_by_alpha(2, my_castle.location, start_location)
+        alpha = get_alpha_from_points(my_castle.location, enemy_castle.location)
+        print alpha
+        if my_castle.location.row > game.rows / 2 and my_castle.location.col < game.cols / 2:
+            alpha = 110
+        else:
+            alpha = 20
+        print alpha
+        location = self.get_object_ring_locations(game, my_castle.location, start_location, 1, 0, alpha)
         if len(location) > 0:
             return location[0]
-        return location
-        #except Exception as msg:
-        #    print msg, "get_next_location"
+        return location  # except Exception as msg:  #    print msg, "get_next_location"
 
     def add_infrastructure(self):
         """
@@ -89,20 +97,71 @@ class Infrastructure(Start):
             portals_sorted = Portals.closest_portals_sorted(game, next_location)
             if len(portals_sorted) > 0:
                 if next_location.distance(portals_sorted[0]) <= self.radius:
-                    self.portal_locations.append(portals_sorted[0].location)
+                    for location in self.portal_locations:
+                        if location.equals(portals_sorted[0].location):
+                            break
+                    else:
+                        self.portal_locations.append(portals_sorted[0].location)
                     self.radius = game.get_my_castle().distance(portals_sorted[0]) + self.radius_delta + 50
                     next_location = next_location = self.get_next_location()
             if self.radius < game.get_my_castle().distance(game.get_enemy_castle()) - self.radius_delta:
                 self.radius += self.radius_delta
             self.ziging_angle *= -1
             self.portal_locations.append(next_location)
-        #except Exception as msg:
-        #    print msg, "add_infrastructure"
+        self.remove_duplicates()  # except Exception as msg:  #    print msg, "add_infrastructure"
 
     def build_and_maintain(self):
-        #try:
+        # try:
         locations = list(self.portal_locations)
         locations.reverse()
-        self.not_built = self.build_structure_ring(self.game, locations, self.elfDict, 0)
-        #except Exception as msg:
-        #    print msg, "build_and_maintain"
+        self.not_built = self.build_structure_ring(self.game, locations, self.elfDict,
+                                                   0)  # except Exception as msg:  #    print msg, "build_and_maintain"
+
+    def get_object_ring_locations(self, game, axis, start_location, amount, object_type=0, alpha=20):
+        """
+        returns a list of where to place a semi ring of objects around a location
+        :param game: the game instance
+        :param axis: the axis (center) around where the object would be built
+        :param start_location: the first location (will be moving to the left and right from this location)
+        :param amount: the amount of location it will return
+        :param object_type: 0 for portals, 1 for fountains
+        :return: a list of objects around the start_location facing end location
+        """
+        target_points = []
+        strt_alpha = get_alpha_from_points(axis, start_location)
+        pos_alpha = strt_alpha + alpha
+        neg_alpha = strt_alpha - alpha
+        while len(
+                target_points) < amount and pos_alpha < strt_alpha + 360 and neg_alpha > strt_alpha - 360:  # while the amount is not reached and the points didnt go a full circle
+
+            pos_point = get_point_by_alpha(pos_alpha % 360, axis, start_location)
+            if object_type == 0 and Portals.portal_on_location(game,
+                                                               pos_point) or object_type == 1 and Normal.Normal.fountain_on_location(
+                game, pos_point):
+                target_points.append(pos_point)
+                print 'pos_point: ' + str(pos_point)
+                if len(target_points) >= amount:
+                    break
+            elif self.can_build_objects_by_list(game, pos_point, target_points, object_type):
+                target_points.append(pos_point)
+                print 'pos_point: ' + str(pos_point)
+                if len(target_points) >= amount:
+                    break
+
+            pos_alpha += 5
+            neg_point = get_point_by_alpha(neg_alpha % 360, axis, start_location)
+            if object_type == 0 and Portals.portal_on_location(game,
+                                                               neg_point) or object_type == 1 and Normal.Normal.fountain_on_location(
+                game, neg_point):
+                target_points.append(neg_point)
+                print 'neg_point: ' + str(neg_point)
+                if len(target_points) >= amount:
+                    break
+            elif self.can_build_objects_by_list(game, neg_point, target_points, object_type):
+                target_points.append(neg_point)
+                print 'neg_point: ' + str(neg_point)
+                if len(target_points) >= amount:
+                    break
+
+            neg_alpha -= 5
+        return target_points
